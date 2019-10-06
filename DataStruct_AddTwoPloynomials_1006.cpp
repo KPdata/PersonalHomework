46b91c255ae19dfe88aaa24eaba67c26767a2479
KPdata <keepdiao17666@gmail.com>
2019-10-06 16:35:45 +0800

    19年国庆数据结构作业

---
+++ DataStruct_AddTwoPloynomials_1006.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include<cstdlib>
+#include <cstring>
+#include<stdlib.h>
+#include <string>
+
+using namespace std;
+
+typedef struct Pfunction
+{
+    double An;//系数
+    double n;//x的次方
+    Pfunction* next;
+
+}Pfunction;
+
+Pfunction* Creat(int num)//num为多项式项的个数。)
+{
+    Pfunction* head;
+    Pfunction* mid;
+    Pfunction* endd;
+
+    head=(Pfunction*)malloc(sizeof(Pfunction));
+    endd=head;//头结点创建。
+
+    for(int i=0;i<num;i++)
+    {
+        mid=(Pfunction*)malloc(sizeof(Pfunction));
+        cin>>mid->An;
+        cin>>mid->n;
+        mid->next=NULL;
+        endd->next=mid;
+        endd=mid;
+        cout<<"多项式第"<<i+1<<"项创建成功"<<endl;
+    }
+    cout<<endl<<"多项式创建成功"<<endl;
+
+    return head;
+}
+
+void display(Pfunction* head)
+{
+    Pfunction* p;
+    p=head->next;
+    while(p!=NULL)
+    {
+        cout<<p->An<<"x^"<<p->n<<"+";
+        p=p->next;
+    }
+}
+
+int main()
+{
+    int n,m;//第一个多项式项数为N，第二个多项式项数为M。
+    Pfunction* PF1;
+    Pfunction* PF2;
+
+    cout<<"Enter the number of two polynomials terms: "<<endl;
+    cin>>n>>m;
+    cout<<"Create the first polynomial"<<endl;
+    PF1=Creat(n);
+    cout<<endl<<"Create the second polynomial"<<endl;
+    PF2=Creat(m);
+
+    cout<<"f(x)=";
+    display(PF1);
+    cout<<endl<<"g(x)=";
+    display(PF2);
+
+    return 0;
+}
